92cf110083a88eb7a7146939502ffe10a7662ef6
rctejon <r.tejon@slangapp.com>
2024-03-17 20:14:03 -0500

    modified:   .gitignore 	new file:   notebooks/citation-network-analysis.ipynb 	modified:   notebooks/evaluation_models.ipynb 	modified:   notebooks/split_ratings.ipynb 	modified:   scripts/generate_ratings_DBLP.py

--- scripts/generate_ratings_DBLP.py
+++ scripts/generate_ratings_DBLP.py
@@ -1,25 +1,44 @@
-import json
+import bigjson
 from tqdm import tqdm
-import math
-
-import pickle
 import os
-
-
-def calculate_rating(progress, threshold=0.8):
-    if progress >= threshold:
-        return 5
-    else:
-        return 5 * math.sqrt(progress / threshold)
-
+import pandas as pd
 
 citations_file_name = "data/DBLP_v12/dblp.v12.json"
+references_dict = {'id':[],
+                  'references':[]}
 
-citations_file = open(citations_file_name, encoding='utf-8')
+if not os.path.exists('data/DBLP_v12/papers_references.feather'):
 
-citations = json.load(citations_file)
+    with open('data/DBLP_v12/dblp.v12.json', 'rb') as f:
+        j = bigjson.load(f)
+        for count in tqdm(range(4894081), total=4894081):
+            element = j[count]
+            if 'references' in element.keys():
+                for i,val in enumerate(element['references']):
+                    references_dict['references'].append(element['references'][i])
+                    references_dict['id'].append(element['id'])
 
-for citation in citations:
-    print(1)
+            else:
+                references_dict['references'].append(-1)
+                references_dict['id'].append(element['id'])
+        papers_references = pd.DataFrame.from_dict(references_dict).astype('int32')
+        papers_references.to_feather("data/DBLP_v12/papers_references.feather")
 
-citations_file.close()
+with open('data/DBLP_v12/papers_references.feather', 'rb') as f:
+    papers_references = pd.read_feather(f)
+    print(papers_references[papers_references['id'] < -1])
+    paper_with_references = papers_references[papers_references['references'] != -1]
+    paper_with_references['rating'] = 1
+    print(paper_with_references)
+
+    train = paper_with_references.sample(frac = 0.8)
+    test = paper_with_references.drop(train.index)
+
+    train.reset_index(drop=True, inplace=True)
+    test.reset_index(drop=True, inplace=True)
+
+    # train.to_feather("data/DBLP_v12/papers_train.feather")
+    # test.to_feather("data/DBLP_v12/papers_test.feather")
+
+
+
